065332aa1e1cd84b5cfe9e6c551684e6bd331fe7
vinicius Fernandes <vinicim.fernandez1@gmail.com>
2021-02-03 22:04:15 -0300

    configurações iniciais do projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule} from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal : true}),
+    TypeOrmModule.forRoot({
+      type : 'postgres',
+      url: process.env.DATABASE_URL,
+     // password: process.env.PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
